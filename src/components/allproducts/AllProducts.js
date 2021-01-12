852d5a7af4eb24b372055811416c5b73d76e8112
Adam Curtis <acurtis1286@gmail.com>
2021-01-12 16:58:19 -0500

    added animation to carousels but need to figure out adding images

--- src/components/allproducts/AllProducts.js
+++ src/components/allproducts/AllProducts.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./AllProducts.css";
 import { db } from "../../firebase";
-import Product from "../product/Product";
+import { AnimatePresence, motion } from "framer-motion";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import CollectionProduct from "./CollectionProduct";
@@ -10,7 +10,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const AllProducts = () => {
-  const [selectedProducts, setSelectedProducts] = useState("mice");
+  const [selectedProducts, setSelectedProducts] = useState("");
   const [products, setProducts] = useState([]);
   const [headsetProducts, setHeadsetProducts] = useState([]);
   const [miceProducts, setMiceProducts] = useState([]);
@@ -110,6 +110,34 @@ const AllProducts = () => {
     //   })
     //   .catch((error) => console.log(error));
   }, []);
+
+  useEffect(() => {
+    console.log("selectedProducts", selectedProducts);
+  }, [selectedProducts]);
+
+  const carouselIn = {
+    initial: {
+      y: "100%",
+      opacity: 0,
+      zIndex: 20,
+    },
+    animate: {
+      y: "0%",
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+    exit: {
+      x: "-100%",
+      zIndex: 10,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
   return (
     <div className="allProducts">
       <Header />
@@ -118,19 +146,25 @@ const AllProducts = () => {
           <div className="allProducts__menuContainer">
             <h3
               onClick={() => setSelectedProducts("headsets")}
-              className="allProducts__menuItem headsets"
+              className={`allProducts__menuItem ${
+                selectedProducts === "headsets" && "headsets"
+              }`}
             >
               Headsets
             </h3>
             <h3
               onClick={() => setSelectedProducts("keyboards")}
-              className="allProducts__menuItem keyboards"
+              className={`allProducts__menuItem ${
+                selectedProducts === "keyboards" && "keyboards"
+              }`}
             >
               Keyboards
             </h3>
             <h3
               onClick={() => setSelectedProducts("mice")}
-              className="allProducts__menuItem mice"
+              className={`allProducts__menuItem ${
+                selectedProducts === "mice" && "mice"
+              }`}
             >
               Mice
             </h3>
@@ -142,69 +176,100 @@ const AllProducts = () => {
             </div>
           ))} */}
         </div>
-        {selectedProducts === "mice" && (
-          <div className="allProducts__products">
-            <Slider {...settings}>
-              {selectedProducts === "mice" &&
-                miceProducts.map((mouse, i) => (
-                  <CollectionProduct
-                    key={i}
-                    image={mouse.image}
-                    brand={mouse.brand}
-                    name={mouse.name}
-                    price={mouse.price}
-                    description1={mouse.description1}
-                    description2={mouse.description2}
-                    description3={mouse.description3}
-                    description4={mouse.description4}
-                    description5={mouse.description5}
-                  />
-                ))}
-            </Slider>
-          </div>
-        )}
-        {selectedProducts === "keyboards" && (
-          <div className="allProducts__products">
-            <Slider {...settings}>
-              {selectedProducts === "keyboards" &&
-                keyboardProducts.map((keyboard, i) => (
-                  <CollectionProduct
-                    key={i}
-                    image={keyboard.image}
-                    brand={keyboard.brand}
-                    name={keyboard.name}
-                    price={keyboard.price}
-                    description1={keyboard.description1}
-                    description2={keyboard.description2}
-                    description3={keyboard.description3}
-                    description4={keyboard.description4}
-                    description5={keyboard.description5}
-                  />
-                ))}
-            </Slider>
-          </div>
-        )}
-        {selectedProducts === "headsets" && (
-          <div className="allProducts__products">
-            <Slider {...settings}>
-              {selectedProducts === "headsets" &&
-                headsetProducts.map((headset, i) => (
-                  <CollectionProduct
-                    key={i}
-                    image={headset.image}
-                    brand={headset.brand}
-                    name={headset.name}
-                    price={headset.price}
-                    description1={headset.description1}
-                    description2={headset.description2}
-                    description3={headset.description3}
-                    description4={headset.description4}
-                    description5={headset.description5}
-                  />
-                ))}
-            </Slider>
-          </div>
-        )}
+        <div className="allProducts__productBox">
+          <AnimatePresence exitBeforeEnter>
+            {selectedProducts === "mice" && (
+              <motion.div
+                key={"mice"}
+                className="allProducts__products"
+                variants={selectedProducts === "mice" && carouselIn}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                <Slider {...settings}>
+                  {selectedProducts === "mice" &&
+                    miceProducts.map((mouse, i) => (
+                      <CollectionProduct
+                        key={i}
+                        image={
+                          "https://drive.google.com/file/d/1HxkXF2a6Xm33WXkq7nZvKIwoGEjrnY8e/view?usp=sharing"
+                        }
+                        brand={mouse.brand}
+                        name={mouse.name}
+                        price={mouse.price}
+                        description1={mouse.description1}
+                        description2={mouse.description2}
+                        description3={mouse.description3}
+                        description4={mouse.description4}
+                        description5={mouse.description5}
+                      />
+                    ))}
+                </Slider>
+              </motion.div>
+            )}
+            {selectedProducts === "keyboards" && (
+              <motion.div
+                key={"keyboards"}
+                className="allProducts__products"
+                variants={selectedProducts === "keyboards" && carouselIn}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                <Slider {...settings}>
+                  {selectedProducts === "keyboards" &&
+                    keyboardProducts.map((keyboard, i) => (
+                      <CollectionProduct
+                        key={i}
+                        image={
+                          "https://drive.google.com/file/d/1HxkXF2a6Xm33WXkq7nZvKIwoGEjrnY8e/view?usp=sharing"
+                        }
+                        brand={keyboard.brand}
+                        name={keyboard.name}
+                        price={keyboard.price}
+                        description1={keyboard.description1}
+                        description2={keyboard.description2}
+                        description3={keyboard.description3}
+                        description4={keyboard.description4}
+                        description5={keyboard.description5}
+                      />
+                    ))}
+                </Slider>
+              </motion.div>
+            )}
+            {selectedProducts === "headsets" && (
+              <motion.div
+                key={"headsets"}
+                className="allProducts__products"
+                variants={selectedProducts === "headsets" && carouselIn}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                <Slider {...settings}>
+                  {selectedProducts === "headsets" &&
+                    headsetProducts.map((headset, i) => (
+                      <CollectionProduct
+                        key={i}
+                        image={
+                          "https://drive.google.com/file/d/1HxkXF2a6Xm33WXkq7nZvKIwoGEjrnY8e/view?usp=sharing"
+                        }
+                        brand={headset.brand}
+                        name={headset.name}
+                        price={headset.price}
+                        description1={headset.description1}
+                        description2={headset.description2}
+                        description3={headset.description3}
+                        description4={headset.description4}
+                        description5={headset.description5}
+                      />
+                    ))}
+                </Slider>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
       <Footer />
     </div>
